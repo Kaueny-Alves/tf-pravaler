1b5144ce5dc90fd341efda585a91b2f2f3645f43
Talita-8 <tlthmf1@outlook.com>
2021-04-12 19:46:21 -0300

    Monetary Mask

--- pravaler/src/pages/Registry/index.js
+++ pravaler/src/pages/Registry/index.js
@@ -1,10 +1,7 @@
-
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
--- pravaler/src/pages/Simulator/index.js
+++ pravaler/src/pages/Simulator/index.js
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
 
 function App() {
-  const [cashStudent, setCashStudent] = useState('')
-  const [moneyLender, setMoneyLender] = useState('')
-  const [result, setResult] = useState('')
-  let resultado = ''
+  const [cashStudent, setCashStudent] = useState("");
+  const [moneyLender, setMoneyLender] = useState("");
+  const [result, setResult] = useState("");
+  let resultado = "";
 
   function Simulator() {
-    const value1 = Number(cashStudent)
-    const value2 = Number(moneyLender)
-    const calculadora = (value1 + value2) / 2.2
-    setResult(calculadora)
+    const value1 = Number(cashStudent);
+    const value2 = Number(moneyLender);
+    const calculadora = (value1 + value2) / 2.2;
+    setResult(calculadora);
   }
 
   return (
@@ -18,22 +18,49 @@ function App() {
       <header className="App-header">
         <h1>Simulador</h1>
 
-        <div class="row">
-          <label class="num1">Renda do Aluno</label>
-          <input id="nome" value={cashStudent} onChange={(e) => setCashStudent(e.target.value)} name="nome" type="text" />
+        <div className="row">
+          <label className="num1">Renda do Aluno</label>
+          <input
+            id="nome"
+            value={cashStudent}
+            onChange={(e) => setCashStudent(e.target.value)}
+            name="nome"
+            type="number"
+          />
         </div>
 
-        <div class="row">
-          <label class="num2">Renda do Garantidor</label>
-          <input id="nome" value={moneyLender} onChange={(e) => setMoneyLender(e.target.value)} name="nome" type="text" />
-          <button id="somar" onClick={(e) => { e.preventDefault(); Simulator() }}>Somar</button>
+        <div className="row">
+          <label className="num2">Renda do Garantidor</label>
+          <input
+            id="nome"
+            value={moneyLender}
+            onChange={(e) => setMoneyLender(e.target.value)}
+            name="nome"
+            type="number"
+          />
+          <button
+            id="somar"
+            onClick={(e) => {
+              e.preventDefault();
+              Simulator();
+            }}
+          >
+            Somar
+          </button>
         </div>
-        <div class="row">
-          <label class="num3">Valor</label>
+        <div className="row">
+          <label className="num3">Valor</label>
           <p>{resultado}</p>
-          <input id="result" value={result} name="result" type="text" />
+          <input
+            id="result"
+            value={Intl.NumberFormat("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+            }).format(result)}
+            name="result"
+            type="text"
+          />
         </div>
-
       </header>
     </div>
   );
