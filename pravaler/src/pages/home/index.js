1618e6451c2be192268d5bf6c04c2c2d225c54bb
RobertaKelly <you@example.com>
2021-04-12 18:46:01 -0300

    fix

--- pravaler/src/pages/home/index.js
+++ pravaler/src/pages/home/index.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
+      <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
