e587bd667ffefc30411d2ec1c38123036070a7cd
Talita-8 <tlthmf1@outlook.com>
2021-04-13 20:47:53 -0300

    Requests functions

--- pravaler/src/pages/login/index.js
+++ pravaler/src/pages/login/index.js
@@ -1,23 +1,40 @@
 /* eslint-disable no-undef */
-import React, {  } from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import Footer from '../../components/footer';
 
-function App() {
+function Login() {
+  const history = useHistory();
+  const routerHome = () => {
+    history.push("/");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header" >
-      <h1>Seja Bem vindo ao nosso Portal</h1>
-      <br></br>
-      <input type='text' className="input-register" placeholder='Insira seu e-mail' onChange={(e) => routerLogin({...signIn, 'email' : e.target.value})} />
-      <br></br>
-      <input type='password' id='password' className="input-register" placeholder='Informe uma senha numerica de 6 digitos' onChange={(e) => routerLogin({...signIn, 'password' : e.target.value})} />
-      <br></br>
-      <div>
-         <br></br>
-           <button id='btn-submit' className="form-input-register" type='submit' value='submit'>Login</button>
-           <div className="texto-cadastro"></div>
-          </div>
+    <>
+      <header>
+        <a href="https://www.pravaler.com.br/" className="brand">
+          <img src="pravaler.png" alt="Logo Pravaler" />
+        </a>
+        <div className="navigation">
+          <button className="btn-clean" onClick={routerHome}>
+            Home
+          </button>
+        </div>
       </header>
-    </div>
+      <section className="login">
+        <h1>Seja bem-vindo ao nosso portal !</h1>
+        <label >E-mail:
+          <input type="text" className="input" placeholder="faculdade@gmailcom" onChange={(e) => routerLogin({...signIn, "email" : e.target.value})} />
+        </label>
+        <label >Senha: 
+          <input type="password" className="input" placeholder='******' onChange={(e) => routerLogin({...signIn, "password" : e.target.value})} />
+        </label>
+        <div>
+          <button className="btn" type="button" value='submit'><b>Login</b></button>
+        </div>
+      </section>
+      <Footer/>
+    </>
   );
 }
-export default App;
+export default Login;
