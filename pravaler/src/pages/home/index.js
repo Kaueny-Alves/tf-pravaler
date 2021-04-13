e16bb412993117f5943658ae13eb02aa0b5a2d72
JulianaAmoriN <juamorim.sn@gmail.com>
2021-04-13 20:01:53 -0300

    ajustes /css Login

--- pravaler/src/pages/home/index.js
+++ pravaler/src/pages/home/index.js
@@ -1,5 +1,3 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-/* eslint-disable jsx-a11y/alt-text */
 import "../../style.css";
 import {
   FaCalculator,
@@ -7,7 +5,7 @@ import {
   FaAlignRight,
   FaFileImport,
 } from "react-icons/fa";
-import students from "../../images/students.jpg";
+import Students from "../../images/students.jpg";
 import { useHistory } from "react-router-dom";
 import Footer from '../../components/footer';
 
@@ -30,23 +28,22 @@ function Home() {
     <>
       <header>
         <a href="https://www.pravaler.com.br/" className="brand">
-          <img src="pravaler.png" alt="" />
+          <img src="pravaler.png" alt="Logo Pravaler"/>
         </a>
-        <div class="menu-btn"></div>
         <div className="navigation">
-          <button className="btn-clean" onClick={routerLogin}>
+          <button type="button "className="btn-clean" onClick={routerLogin}>
             Portal
           </button>
-          <button className="btn" onClick={routerRegistry}>
+          <button type="button" className="btn" onClick={routerRegistry}>
             Cadastre-se
           </button>
         </div>
       </header>
-      <section className="about" id="about">
+      <section className="about">
         <div className="content">
           <div className="column col-left">
             <div className="img-card">
-              <img src={students} />
+              <img src={Students} alt="Imagem estudante"/>
             </div>
           </div>
           <div className="column col-right">
@@ -54,9 +51,9 @@ function Home() {
               Seu futuro você faz acontecer. Invista na sua educação com o
               Pravaler.
             </h2>
-            <a className="btn" onClick={routerSimulator}>
+            <button className="btn" onClick={routerSimulator}>
               <b>Simule agora</b>
-            </a>
+            </button>
             <h3>Faça tudo 100% online</h3>
             <p className="paragraph-text">
               Em apenas 4 passos você pode garantir o seu financiamento
@@ -68,7 +65,7 @@ function Home() {
                 <h4>Simule seu financiamento</h4>
                 <span>
                   Encontre a Instituição, curso e depois veja valores na
-                  simulação
+                  simulação.
                 </span>
               </div>
               <div className="card">
@@ -100,7 +97,6 @@ function Home() {
         </div>
       </section>
       <Footer/>
-     
     </>
   );
 }
--- pravaler/src/pages/login/index.js
+++ pravaler/src/pages/login/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
-import React, { } from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import Footer from '../../components/footer';
 
@@ -11,11 +11,10 @@ function Login() {
 
   return (
     <>
-    <header>
+      <header>
         <a href="https://www.pravaler.com.br/" className="brand">
-          <img src="pravaler.png" alt="" />
+          <img src="pravaler.png" alt="Logo Pravaler" />
         </a>
-        <div class="menu-btn"></div>
         <div className="navigation">
           <button className="btn-clean" onClick={routerHome}>
             Home
@@ -23,15 +22,16 @@ function Login() {
         </div>
       </header>
       <section className="login">
-      <h1>Seja bem-vindo ao nosso portal !</h1>
-      <label >E-mail:</label>
-      <input type='text' className="input" placeholder='faculdade@gmailcom' onChange={(e) => routerLogin({...signIn, 'email' : e.target.value})} />
-      <label >Senha: </label>
-      <input type='password' className="input" placeholder='******' onChange={(e) => routerLogin({...signIn, 'password' : e.target.value})} />
-      <div>
-           <button className="btn" type='submit' value='submit'><b>Login</b></button>
-           <div className="texto-cadastro"></div>
-          </div>
+        <h1>Seja bem-vindo ao nosso portal !</h1>
+        <label >E-mail:
+          <input type="text" className="input" placeholder="faculdade@gmailcom" onChange={(e) => routerLogin({...signIn, "email" : e.target.value})} />
+        </label>
+        <label >Senha: 
+          <input type="password" className="input" placeholder='******' onChange={(e) => routerLogin({...signIn, "password" : e.target.value})} />
+        </label>
+        <div>
+          <button className="btn" type="button" value='submit'><b>Login</b></button>
+        </div>
       </section>
       <Footer/>
     </>
